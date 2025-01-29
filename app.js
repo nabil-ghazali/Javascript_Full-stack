c934fd7344c5fded049ea7b471d1471c406d5803
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2025-01-29 20:15:41 +0100

    Configuration de la dataBase MongoDB, connection de l'API sur mon Cluster MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://nabilCastelnau:Taratata1986@sellobject.2mjxb.mongodb.net/?retryWrites=true&w=majority&appName=sellObject',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,7 +18,12 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/stuff', (req, res, next) => {
+app.post('/api/stuff',(req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({message: 'Objet créé !'});
+})
+
+app.get('/api/stuff', (req, res, next) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
