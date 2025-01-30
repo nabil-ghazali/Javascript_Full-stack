f03e9b116f5287ef35712a6edfb986a761e303fb
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2025-01-30 21:29:05 +0100

    Création de la route post

--- models/Thing.js
+++ models/Thing.js
@@ -1,5 +1,7 @@
+// Création d'un schéma de données pour les objets Thing avec Mongoose
 const mongoose = require('mongoose');
 
+// La méthode 'Schema' de mongoose nous permet de créer un schéma de données qui contient les champs souhaités pour chaque objet de type Thing.
 const thingSchema = mongoose.Schema({
     title: { type: String, required:true },
     description: {type: String, required: true},
@@ -8,4 +10,5 @@ const thingSchema = mongoose.Schema({
     userId: {type: String, required: true},
 });
 
+// La méthode 'model' de mongoose nous permet de créer un modèle et de le rendre disponible pour notre application.
 module.exports = mongoose.model('Thing', thingSchema);
