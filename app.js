0fa3d950eec84993f8012a53f1558df2a0b9cf2e
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2025-02-02 22:36:13 +0100

    Création rout get pour récupérer un seul objet

--- app.js
+++ app.js
@@ -33,7 +33,16 @@ app.post('/api/stuff',(req, res, next) => {
     .catch(error => res.status(400).json({error}));
 })
 
-app.use('/api/stuff', (req, res, next) => {
+// Route permettant de récupérer un objet Thing spécifique
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+         .then(thing => res.status(200).json(thing))
+         .catch(error => res.status(404).json({error}))
+
+ });
+
+ // Route permettant de récupérer tous les objets Thing de la base de données
+app.get('/api/stuff', (req, res, next) => {
    Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({error}))
