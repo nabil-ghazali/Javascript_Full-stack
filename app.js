f838883268405319c8fc632607b7f72daf64f3cf
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2025-02-04 18:43:09 +0100

    Création route put et delete

--- app.js
+++ app.js
@@ -49,6 +49,20 @@ app.get('/api/stuff', (req, res, next) => {
 
   });
 
+ // Route permettant de modifier un objets Thing de la base de données
+app.put('/api/stuff/:id', (req, res, next) => {
+   Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({error}))
 
+  });
+
+  // Route permettant de supprimer un objets Thing de la base de données
+  app.delete('/api/stuff/:id', (req, res, next) => { 
+    Thing.deleteOne({_id: req.params.id}, {_id: req.params.id}) 
+      .then(()=> res.status(200).json({message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({error}))
+  
+  });
 module.exports = app;
 
